4ed9563a8d4b62d6223d29a6736504cf8d2fd2e5
Tyrone <tvalverdes9@gmail.com>
2023-10-30 18:43:51 -0500

    first commit

---
+++ src/main/java/com/practica/demo/DemoApplication.java
@@ -0,0 +1,33 @@
+package com.practica.demo;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.practica.demo.student.Student;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	public List<Student> hello() {
+		return List.of(new Student(
+			1L,
+			"Mariam",
+			"mariam@gmail.com",
+			LocalDate.of(2000, Month.JANUARY, 5)
+			,21
+		));
+	}
+
+}
